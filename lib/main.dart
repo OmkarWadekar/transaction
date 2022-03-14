2978d972a9f3aa49ecd4de9c8e36714bdbd02428
OmkarWadekar <wadekaromkar08@gmail.com>
2022-03-14 15:31:34 +0530

    first

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp( transaction_home());
+}
+
+class transaction_home extends StatefulWidget {
+  const transaction_home({Key? key}) : super(key: key);
+
+  @override
+  _transaction_homeState createState() => _transaction_homeState();
+}
+
+class _transaction_homeState extends State<transaction_home> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        
+      ),
+    );
+  }
+}
+
+
+
